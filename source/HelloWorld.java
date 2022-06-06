26b35fe684cb593a77445e862deb74baa64279df
ljendrusch <89661584+ljendrusch@users.noreply.github.com>
2022-06-06 12:02:10 -1000

    Update HelloWorld.java

--- source/HelloWorld.java
+++ source/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("changed");
-        System.out.println("Hello world");
+        System.out.println("changed"); // delete line
+        System.out.println("Hello world"); // make pretty
     }
 }
